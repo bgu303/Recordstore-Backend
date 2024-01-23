a228cafc9285059c1d9f10d21a98de023e24fa87
bgu303 <bgu303@myy.haaga-helia.fi>
2024-01-23 13:22:41 +0200

    User related stuff added.

--- routes/records.js
+++ routes/records.js
@@ -1,35 +1,35 @@
 const express = require("express");
 const router = express.Router();
-const dbConnection = require("../databaseconnection/databaseconnection")
+const dbConnection = require("../databaseconnection/databaseconnection");
 
 router.get("/", (req, res) => {
-    const query = "SELECT * FROM rec"
+    const query = "SELECT * FROM rec";
 
     dbConnection.query(query, (error, results) => {
         if (error) {
-            console.log(error)
+            console.log(error);
             return res.status(500).json({ error: "Internal Server Error" });
         } else {
-            res.json(results)
+            res.json(results);
         }
     })
 })
 
 router.delete("/:id", (req, res) => {
-    const recordId = req.params.id
-    const query = "DELETE FROM rec WHERE id = ?"
+    const recordId = req.params.id;
+    const query = "DELETE FROM rec WHERE id = ?";
 
     dbConnection.query(query, [recordId], (error, results) => {
         if (error) {
-            console.log(error)
-            return res.status(500).json({ error: "Internal Server Error" })
+            console.log(error);
+            return res.status(500).json({ error: "Internal Server Error" });
         }
         if (results.affectedRows === 0) {
             return res.status(404).json({ error: "Record not found" });
         }
         res.json({ success: true, message: "Record deleted successfully" });
-        console.log(`Record deleted with id: ${recordId}`)
+        console.log(`Record deleted with id: ${recordId}`);
     })
 })
 
-module.exports = router
+module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const dbConnection = require("../databaseconnection/databaseconnection");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 router.use(express.json());
 
 router.post("/createuser", async (req, res) => {
@@ -9,19 +10,19 @@ router.post("/createuser", async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const query = "INSERT INTO recordstoreusers (email, user_password, user_role) VALUES (?, ?, ?)";
-    const values = [email, hashedPassword, role]
+    const values = [email, hashedPassword, role];
 
     dbConnection.query(query, values, (error, results) => {
 
         if (error.errno === 1062) {
-            return res.status(501).json({ error: "Internal Server Error."})
+            return res.status(501).json({ error: "Internal Server Error."});
         }
         if (error) {
             console.log(error);
-            return res.status(500).json({ error: "Internal Server Error."})
+            return res.status(500).json({ error: "Internal Server Error."});
         }
         if (results.affectedRows === 1) {
-            console.log("User created successfully.")
+            console.log("User created successfully.");
             return res.status(201).json({ success: true, message: "User created successfully" });
         } else {
             return res.status(500).json({ error: "Failed to create user" });
@@ -29,4 +30,32 @@ router.post("/createuser", async (req, res) => {
     })
 })
 
-module.exports = router
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    const query = "SELECT * FROM recordstoreusers WHERE email = ?";
+
+    dbConnection.query(query, [email], async (error, results) => {
+        if (error) {
+            console.log(`Error in logging in: ${error}`);
+            return res.status(500).json({ error: "Internal Server Error" });
+        }
+
+        if (results.length === 0) {
+            return res.status(401).json({ error: "Invalid email or password" });
+        }
+
+        const user = results[0];
+        const passwordChecker = await bcrypt.compare(password, user.user_password);
+
+        if (!passwordChecker) {
+            return res.status(401).json({ error: "Invalid email or password" });
+        }
+    
+        const token = jwt.sign({ userId: user.id, email: user.email, user_role: user.user_role }, 'your-secret-key', { expiresIn: '1h' });
+        console.log(`Logging in with token: ${token}`);
+        res.status(200).json({ success: true, token });
+
+    })
+})
+
+module.exports = router;
