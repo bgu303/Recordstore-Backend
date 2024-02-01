383d036e84fad82c6d23f7410057a53eecb9a9ba
bgu303 <bgu303@myy.haaga-helia.fi>
2024-02-01 12:16:45 +0200

    Sending shoppingcart with email implemented.

--- routes/records.js
+++ routes/records.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const dbConnection = require("../databaseconnection/databaseconnection");
+const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
 
 router.get("/", (req, res) => {
     const query = "SELECT * FROM rec";
@@ -69,13 +70,13 @@ router.delete("/shoppingcartdeleteall/:id", (req, res) => {
     dbConnection.query(query, [userId], (error, results) => {
         if (error) {
             console.log(error);
-            return res.status(500).json({ error: "Internal Server Error. "});
+            return res.status(500).json({ error: "Internal Server Error. " });
         }
         if (results.affectedRows === 0) {
             console.log("Nothing deleted");
             return res.status(404).json({ error: "Nothing deleted." });
         }
-        res.json({ success: true, message: "Records deleted successfully."});
+        res.json({ success: true, message: "Records deleted successfully." });
         console.log("Records deleted successfully");
     })
 })
@@ -104,17 +105,72 @@ router.post("/addtocart", (req, res) => {
 router.post("/addnewrecord", (req, res) => {
     const { artist, title, label, size, lev, kan, price, genre, discogs } = req.body;
     const query = "INSERT INTO rec (artist, title, label, size, lev, kan, price, genre, discogs) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    const values = [artist, title, label, size, lev, kan, price, genre, discogs] ;
+    const values = [artist, title, label, size, lev, kan, price, genre, discogs];
 
     dbConnection.query(query, values, (error, results) => {
         if (error) {
-            return res.status(501).json({ error: "Internal Server Error. "});
+            return res.status(501).json({ error: "Internal Server Error. " });
         }
         if (results.affectedRows === 1) {
             console.log("New record added successfully");
             return res.status(201).json({ success: true, message: "New record added successfully" });
         }
     })
+})
+
+router.post("/sendcart", (req, res) => {
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(SENDGRID_API_KEY);
+
+    const { shoppingcart, customerInfo } = req.body;
+    let totalPrice = 0;
+
+    let orderInfo = `
+        Tilaajan nimi: ${customerInfo.name}
+        Tilaajan puhelinnumero: ${customerInfo.phoneNumber}
+        Tilaajan sähköposti: ${customerInfo.email}
+        Tilaajan osoite: ${customerInfo.address}
+        Tilaajan postinumero: ${customerInfo.zipCode}
+        Tilaajan kaupunki: ${customerInfo.city}
+    `
+
+    const formattedItems = shoppingcart.map(item => {
+        totalPrice += item.price
+        return `
+        Artisti: ${item.artist}
+        Levyn nimi: ${item.title}
+        Koko: ${item.size}
+        Hinta: ${item.price}
+        ---------------------
+        `;
+    }).join('');
+
+    const textContent = `
+        Tilaajan tiedot:
+        ${orderInfo}
+
+        Ostoskorin sisältö:
+        ${formattedItems}
+
+        Yhteishinta:
+        ${totalPrice} euroa.
+    `;
+
+    const msg = {
+        to: 'jukkavesanto93@gmail.com',
+        from: 'mivesstore@gmail.com',
+        subject: 'Ostoskori',
+        text: textContent
+    }
+    sgMail
+        .send(msg)
+        .then(() => {
+            console.log('Email sent')
+            return res.status(201).json({ success: true, message: "Shoppingcart send successfully" });
+        })
+        .catch((error) => {
+            console.error(error)
+        })
 })
 
 module.exports = router;
