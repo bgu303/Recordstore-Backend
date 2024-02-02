6df30e52a871a2dd3131cfe890423a7f0fecf4ba
bgu303 <bgu303@myy.haaga-helia.fi>
2024-02-02 15:58:18 +0200

    Updated schema. Added AWS RDS connection.

--- databaseconnection/databaseconnection.js
+++ databaseconnection/databaseconnection.js
@@ -1,13 +1,16 @@
 const mysql = require("mysql");
 const username = process.env.DATABASE_USERNAME
 const password = process.env.DATABASE_PASSWORD
+const usernameCloud = process.env.DATABASE_CLOUD_USERNAME
+const passwordCloud = process.env.DATABASE_CLOUD_PASSWORD
+const hostname = process.env.HOSTNAME_CLOUD
 
 const dbConnection = mysql.createConnection({
-    host: "localhost",
-    user: username,
-    password: password,
+    host: hostname,
+    user: usernameCloud,
+    password: passwordCloud,
     port: 3306,
-    database: "webstoredatabase"
+    database: "recordstore"
 });
 
 module.exports = dbConnection
