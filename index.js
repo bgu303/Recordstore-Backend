aa0f7d2aa55093266c78b840c10e979403fb5d3f
bgu303 <bgu303@myy.haaga-helia.fi>
2024-03-05 15:20:00 +0200

    socket.io implementation started.

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const express = require("express");
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const http = require("http");
+const socketIo = require("socket.io");
 const port = 3001
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+      origin: "http://localhost:3000", // Allow requests from this origin, remember to change when cloud implemented.
+      methods: ["GET", "POST"] // Allow only specified methods
+    }
+  });
 
 const recordsRouter = require("./routes/records")
 const userRouter = require("./routes/user")
@@ -17,6 +26,13 @@ app.use("/user", userRouter)
 app.use("/shoppingcart", shoppingcartRouter)
 app.use("/chat", chatRouter)
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    socket.on("sendMessage", (message) => {
+        console.log(`Message was sent ${message}`);
+        io.emit("message", message)
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
