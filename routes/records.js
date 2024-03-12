c6da59b6d2c23ab0bd9644ca1eb808b722ca5173
bgu303 <bgu303@myy.haaga-helia.fi>
2024-03-12 15:46:51 +0200

    god help me with these errors.

--- routes/records.js
+++ routes/records.js
@@ -8,7 +8,7 @@ router.get("/", (req, res) => {
     dbConnection.query(query, (error, results) => {
         if (error) {
             console.log(error);
-            return res.status(500).json({ error: "Internal Server Error" });
+            return res.status(500).json({ error: "Internal Server Error" + error});
         } else {
             res.json(results);
         }
