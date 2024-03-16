411f0274244a78a5741a45ce8988dfbdcaeb3b69
bgu303 <bgu303@myy.haaga-helia.fi>
2024-03-16 17:31:20 +0200

    hehe

--- databaseconnection/databaseconnection.js
+++ databaseconnection/databaseconnection.js
@@ -7,11 +7,11 @@ const databaseNameCloud = process.env.DATABASE_CLOUD_DATABASENAME
 const hostname = process.env.HOSTNAME_CLOUD
 
 const dbConnection = mysql.createConnection({
-    host: "localhost",
-    user: username,
-    password: password,
+    host: hostname,
+    user: usernameCloud,
+    password: passwordCloud,
     port: 3306,
-    database: "webstoredatabase"
+    database: databaseNameCloud
 });
 
 module.exports = dbConnection
