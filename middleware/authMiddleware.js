5d244f5205a5933320a1f95da54744c40fdaa944
bgu303 <bgu303@myy.haaga-helia.fi>
2024-04-29 15:22:21 +0300

    Orders working as intended.

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
+    console.log(token)
     
     if (!token) {
         return res.status(403).json({ error: "Invalid token" });
@@ -17,5 +18,29 @@ function authenticateToken(req, res, next) {
     });
 }
 
-module.exports = authenticateToken;
+function authenticateAdminToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    console.log(token)
+    
+    if (!token) {
+        return res.status(403).json({ error: "Invalid token" });
+    }
+
+    jwt.verify(token, 'your-secret-key', (err, user) => {
+        if (err) {
+            return res.status(403).json({ error: "Invalid token" });
+        }
+        if (user.userRole !== 'ADMIN') {
+            return res.status(401).json({ error: "Unauthorized: Only admins can access this resource" });
+        }
+        req.user = user;
+        next();
+    });
+}
+
+module.exports = {
+    authenticateToken,
+    authenticateAdminToken
+};
 
--- routes/shoppingcart.js
+++ routes/shoppingcart.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const dbConnection = require("../databaseconnection/databaseconnection");
 const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
-const authenticateToken = require("../middleware/authMiddleware");
+const { authenticateToken, authenticateAdminToken } = require("../middleware/authMiddleware");
 
 router.get("/shoppingcartitems/:id", authenticateToken, (req, res) => {
     const userId = req.params.id;
@@ -173,7 +173,7 @@ router.post("/sendcart", (req, res) => {
     })
 })
 
-router.get("/getorderdata", (req, res) => {
+router.get("/getorderdata", authenticateAdminToken, (req, res) => {
 
     //GPT-magic. :) The query returns all of the order items with the order details aswell. So, grouping is needed on front-end side to only show customer data once, then item data.
     const query = `SELECT o.*, oi.record_id, r.artist, r.title, r.size, r.price
@@ -187,11 +187,46 @@ router.get("/getorderdata", (req, res) => {
             console.log(error);
             return res.status(501).json({ error: "Internal Server Error." });
         } else {
-            console.log(results)
             res.json(results);
         }
     });
 });
 
+router.delete("/deleteorder/:id", authenticateAdminToken, (req, res) =>{
+    const orderId = req.params.id;
+    const getOrderItemsQuery = "SELECT record_id FROM order_items WHERE order_id = ?"
+    const deleteOrderQuery = "DELETE FROM orders WHERE id = ?";
+    const updateOrderItemsQuery = "UPDATE rec SET sold = false WHERE id IN (?)";
+
+    //First gets the record ids associated with the order (used to set status later)
+    //Secondly, the order is deleted (order items will be deleted on order deletion)
+    //Third, updates the status of sold items of the order back to false, based on the fetched record ids in the first step.
+    dbConnection.query(getOrderItemsQuery, [orderId], (error, results) => {
+        const recordIds = results.map(row => row.record_id);
+        
+        dbConnection.query(deleteOrderQuery, [orderId], (error, results) => {
+            if (error) {
+                console.log(error);
+                return res.status(500).json({ error: "Internal Server Error." });
+            }
+            if (results.affectedRows === 0) {
+                console.log("No order deleted");
+                return res.status(404).json({ error: "No order deleted." });
+            }
+            console.log(`Order deleted with id: ${orderId}`);
+            
+            dbConnection.query(updateOrderItemsQuery, [recordIds], (error, results) => {
+                if (error) {
+                    console.log(error);
+                    return res.status(500).json({ error: "Internal Server Error." });
+                }
+
+                console.log("Records updated successfully.");
+
+                res.json({ success: true, message: "Order deleted successfully." });
+            });
+        });
+    });
+});
 
 module.exports = router;
