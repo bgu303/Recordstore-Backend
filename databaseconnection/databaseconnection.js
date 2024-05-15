617066f37bd00081a48c3fea919e96da27a27fbe
bgu303 <bgu303@myy.haaga-helia.fi>
2024-05-15 12:16:30 +0300

    Back to local deployment.

--- databaseconnection/databaseconnection.js
+++ databaseconnection/databaseconnection.js
@@ -1,17 +1,18 @@
 const mysql = require("mysql");
 const username = process.env.DATABASE_USERNAME
 const password = process.env.DATABASE_PASSWORD
+const databaseName = process.env.DATABASE_DATABASENAME
 const usernameCloud = process.env.DATABASE_CLOUD_USERNAME
 const passwordCloud = process.env.DATABASE_CLOUD_PASSWORD
 const databaseNameCloud = process.env.DATABASE_CLOUD_DATABASENAME
 const hostname = process.env.HOSTNAME_CLOUD
 
 const dbConnection = mysql.createConnection({
-    host: hostname,
-    user: usernameCloud,
-    password: passwordCloud,
+    host: "localhost",
+    user: username,
+    password: password,
     port: 3306,
-    database: databaseNameCloud
+    database: databaseName
 });
 
 module.exports = dbConnection
